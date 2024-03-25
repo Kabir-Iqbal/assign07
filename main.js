91d4bdd62b55cb225b8ad9433a4f9cd25c970c1e
Kabir-iqbal <161919126+Kabir-Iqbal@users.noreply.github.com>
2024-03-25 19:50:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+// 7. Number Eight: 
+// Write addition, subtraction, multiplication, and division operations that each result in the 
+// number 8. Be sure to enclose your operations in print statements to see the results.
+let addition = 5 + 3;
+console.log(`Addition : ${addition}`);
+let subtraction = 10 - 2;
+console.log(`Substraction : ${subtraction}`);
+let multiplication = 4 * 2;
+console.log(`Multiplication : ${multiplication}`);
+let division = 16 / 2;
+console.log(`Division : ${division}`);
